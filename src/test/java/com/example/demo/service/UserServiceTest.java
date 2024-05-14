74a117745fb847315aa3173b02c8768a637ba43e
Space <enaenen@naver.com>
2024-05-14 22:33:17 +0900

    UserService tests

--- src/test/java/com/example/demo/service/UserServiceTest.java
+++ src/test/java/com/example/demo/service/UserServiceTest.java
@@ -2,13 +2,22 @@ package com.example.demo.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 
+import com.example.demo.exception.CertificationCodeNotMatchedException;
 import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.UserStatus;
+import com.example.demo.model.dto.UserCreateDto;
+import com.example.demo.model.dto.UserUpdateDto;
 import com.example.demo.repository.UserEntity;
+import java.security.cert.CertificateException;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
@@ -23,6 +32,8 @@ import org.springframework.test.context.jdbc.SqlGroup;
 public class UserServiceTest {
   @Autowired
   private UserService userService;
+  @MockBean
+  private JavaMailSender javaMailSender;
 
   @Test
   void getByEmail은_ACTIVE_상태인_유저를_찾아올_수_있다(){
@@ -66,4 +77,77 @@ public class UserServiceTest {
         .isInstanceOf(ResourceNotFoundException.class);
   }
 
+
+  @Test
+  void userCreateDto_를_이용하여_유저를_생성할_수_있다(){
+    //given
+    UserCreateDto userCreateDto = UserCreateDto.builder()
+        .email("enaenen@naver.com")
+        .address("서울시 강남구")
+        .nickname("enaenen")
+        .build();
+    BDDMockito.doNothing().when(javaMailSender).send(any(SimpleMailMessage.class));
+
+    //when
+    UserEntity result = userService.create(userCreateDto);
+    //then
+    assertThat(result.getId()).isNotNull();
+    assertThat(result.getStatus()).isEqualTo(UserStatus.PENDING);
+//    assertThat(result.getCertificationCode()).isEqualTo("???");
+  }
+
+
+  @Test
+  void userCreateDto_를_이용하여_유저를_수정할_수_있다(){
+    //given
+    UserUpdateDto userUpdateDto = UserUpdateDto.builder()
+        .address("경기도 성남시")
+        .nickname("enaenen-1")
+        .build();
+
+    //when
+   userService.update(1, userUpdateDto);
+
+    //then
+    UserEntity userEntity = userService.getById(1);
+    assertThat(userEntity.getId()).isNotNull();
+    assertThat(userEntity.getAddress()).isEqualTo("경기도 성남시");
+    assertThat(userEntity.getNickname()).isEqualTo("enaenen-1");
+  }
+
+
+  @Test
+  void user를_로그인_시키면_마지막_로그인_시간이_변경된다(){
+    //given
+    //when
+    userService.login(1);
+
+    //then
+    UserEntity userEntity = userService.getById(1);
+    assertThat(userEntity.getLastLoginAt()).isGreaterThan(0L);
+//    assertThat(userEntity.getLastLoginAt()).isEqualTo("..."); // fixme
+  }
+
+
+  @Test
+  void PENDING_상태의_사용자는_인증_코드로_ACTIVE_시킬_수_있다(){
+    //given
+    //when
+    userService.verifyEmail(2,"aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab");
+
+    //then
+    UserEntity userEntity = userService.getById(2);
+    assertThat(userEntity.getStatus()).isEqualTo(UserStatus.ACTIVE);
+//    assertThat(userEntity.getLastLoginAt()).isEqualTo("..."); // fixme
+  }
+
+  @Test
+  void PENDING_상태의_사용자는_잘못된_인증_코드를_받으면_에러를_던진다(){
+    //given
+    //when
+    //then
+    assertThatThrownBy(() -> {
+      userService.verifyEmail(2,"aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaC");
+    }).isInstanceOf(CertificationCodeNotMatchedException.class);
+  }
 }
